e59711f36e0b85f353caeb3e37407ce789d47e53
Miroslav Kouril <miroslav.kouril@atptour.com>
2024-06-13 10:25:15 +0100

    Demo app with the nugets

---
+++ ATPWTAJointApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ATPWTAJointApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ ATPWTAJointApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ATPWTAJointApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
+
